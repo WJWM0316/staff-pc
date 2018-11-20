84b32641dbfebb0ed2d2b2d2b7eec471f552c4bf
lizifa <lphvali@thetiger.com.cn>
2018-11-20 18:14:11 +0800

    测试

--- src/router/routes.js
+++ src/router/routes.js
@@ -14,5 +14,20 @@ export const routes = [
       // 页面是否需要缓存
       keepAlive: false
     }
+  },
+  {
+    // 路由的路径
+    path: '/index',
+    // 路由名称
+    name: 'index',
+    // 页面的标题
+    title: '首页',
+    // 引用的组件
+    component: () => import(/* webpackChunkName: "index" */ '@/pages/index/index.vue'),
+    // 元信息
+    meta: {
+      // 页面是否需要缓存
+      keepAlive: false
+    }
   }
 ]
