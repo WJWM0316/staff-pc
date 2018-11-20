d500bfe8bd991a812eebbdd97512241debc3c143
lizifa <lphvali@thetiger.com.cn>
2018-11-20 15:58:11 +0800

    测试vuex

---
+++ src/components/nodata/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="nodata">
+    <div class="nodata-inner">
+      <img src="~IMAGES/no-data.png" alt="">
+      <p>{{defaultText}}</p>
+    </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+	name: 'nodata',
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    defaultText() {
+      return this.text
+    }
+  }
+})
+</script>
+<style lang="scss" scoped>
+	
+</style>
