65e90477afe509c6a76d49876320449ae1d2f02e
lizifa <lphvali@thetiger.com.cn>
2018-11-21 10:29:31 +0800

    修改路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import { routes } from './routes.js'
+import Cookies from 'js-cookie'
 
 Vue.use(Router)
-const base = location.href.split('/')[3]
 
 export default new Router({
 	mode: 'history',
-	base,
+	base: process.env.NODE_ENV === 'development' ? process.env.VUE_APP__TEST_COMPANY : Cookies.get('code'),
   routes
 })
