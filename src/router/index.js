dda6f375b8be5cb9a6b5fb33dbd66a327f1fcf80
lizifa <lphvali@thetiger.com.cn>
2018-11-21 10:25:52 +0800

    测试路由

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import Router from 'vue-router'
 import { routes } from './routes.js'
 
 Vue.use(Router)
+const base = location.href.split('/')[3]
 
 export default new Router({
 	mode: 'history',
-	base: process.env.NODE_ENV === 'production' ? `/${process.env.VUE_APP_COMPANY}` : '/',
+	base,
   routes
 })
