c8dafa04a50be326ed3576f561519f5be5b356ed
lizifa <lphvali@thetiger.com.cn>
2018-11-22 12:07:22 +0800

    加几个接口

--- src/store/api/jobcircle.js
+++ src/store/api/jobcircle.js
@@ -12,5 +12,14 @@ export const nofocusJobCircleApi = params => request('/jobcircle/nofocus', 'put'
 // 获取全部可见的工作圈列表
 export const getAllVisibleJobcircleApi = params => request('/jobcircle/all', 'get', params)
 
-// 获取全部可见的工作圈列表
-export const getAllVisibleJobci1rcleApi = params => request(`/staff/postaffix/`, 'get', params)
+// 搜索当前工作圈内的带有文件、图片、视频、链接的帖子
+export const getJobcirclePostaffixApi = params => request(`/staff/postaffix/${params.id}`, 'get', params.params)
+
+// 获取工作圈分月相册
+export const getJobcirclePostaffixOfPictureApi = params => request(`/jobcircle/picture/${params.id}?month=${params.month}`, 'get', params.params)
+
+// 获取工作圈所有帖子文件分页
+export const getJobcirclePostaffixOfFilesApi = params => request(`/jobcircle/files/${params.id}`, 'get', params.params)
+
+// 获取工作圈所有帖子链接分页
+export const getJobcirclePostaffixOfUrlsApi = params => request(`/jobcircle/urls/${params.id}`, 'get', params.params)
