268db7c3732aac795051f5039d9882ddf4c9b6c8
lizifa <lphvali@thetiger.com.cn>
2018-11-23 11:26:56 +0800

    提交图片

---
+++ src/components/commentBox/index.vue
@@ -0,0 +1,15 @@
+<template>
+	<section id="comment-box"></section>
+</template>
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component({
+	name: 'comment-box'
+})
+export default class ComponentCommentBox extends Vue {}
+</script>
+<style lang="scss">
+.comment-box {}
+</style>
